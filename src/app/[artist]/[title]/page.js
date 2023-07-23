1ce6e749bba0288976291bcdfd0f2511b23dddf4
Kiriyako <doraemonboy158@gmail.com>
2023-07-23 17:21:19 +0530

    yes

---
+++ src/app/[artist]/[title]/page.js
@@ -0,0 +1,59 @@
+import { getLyrics, getSong } from 'genius-lyrics-api';
+import { Maven_Pro } from 'next/font/google'
+import { Noto_Sans_JP } from 'next/font/google';
+const maven = Maven_Pro({subsets: ['latin'], weight: ['400']}) 
+const sansjp = Noto_Sans_JP({subsets: ['latin'], weight: ['600']}) 
+
+export default function Main({params}) {
+    let artist;
+    if (params.artist === 'noArtist') {
+      artist = ' ';
+    } else {
+      artist = decodeURIComponent(params.artist);
+    }
+    const title = decodeURIComponent(params.title);
+  const options = {
+    apiKey: '4D-pz_Kz7UX_3scJ01mt7oiZ5wQYULf_cNZ5tqIWKyYIHpwnp1l2WWw7KVD9C1Up',
+    title: `${artist}`,
+    artist: `${title}`,
+    optimizeQuery: true,
+  };
+
+  return (
+    <div id="content">
+    <div
+    className={maven.className}
+      style={{
+        maxWidth: '600px',
+        margin: '0 auto',
+        padding: '20px',
+        backgroundColor: '#70707080',
+        boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center', 
+        color: 'white',
+        borderRadius: '15px',
+      }}
+    >
+      {getSong(options).then((song) => (
+        <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+          <img
+            src={song.albumArt}
+            alt="Album Art"
+            style={{
+              maxWidth: '200px',
+              borderRadius: '4px',
+              boxShadow: '0 0 5px rgba(0, 0, 0, 0.2)',
+               margin: '0 auto',
+            }}
+          />
+          <p style={{ fontSize: '18px' }}>{song.title}</p>
+        </div>
+      ))}
+      {getLyrics(options).then((lyrics) => (
+        <pre     className={sansjp.className}   style={{textAlign: 'center', whiteSpace: 'pre-wrap', fontSize: '18px', lineHeight: '1.5' }}>{lyrics}</pre>
+      ))}
+    </div></div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,96 @@
-import Image from 'next/image'
+"use client";
+import React, { useEffect, useRef, useState } from "react"; 
+import { Maven_Pro } from "next/font/google";
+import { useRouter } from 'next/navigation';
 
-export default function Home() {
+const maven = Maven_Pro({ subsets: ["latin"], weight: ["400"] });
+
+export default function Main() {
+  const artistRef = useRef("");
+  const nameRef = useRef("");
+  const router = useRouter();
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  useEffect(() => {
+    if (formSubmitted) {
+      if (nameRef.current.trim() === "") {
+        alert('Please enter a song name!');
+      } else if (artistRef.current.trim() === "") {
+        router.push(`/noArtist/${nameRef.current}`);
+      } else {
+        router.push(`/${artistRef.current}/${nameRef.current}`);
+      }
+    }
+  }, [formSubmitted, router]);
+
+  function submitForm(e) {
+    e.preventDefault();
+    setFormSubmitted(true);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div
+      className={maven.className}
+      style={{
+        maxWidth: "600px",
+        margin: "0 auto",
+        padding: "20px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        color: "white",
+        borderRadius: "15px",
+      }}
+      id="main"
+    >
+      <h1 style={{ fontSize: "30px" }}>ayakaa</h1>
+      <h1 style={{ fontSize: "20px" }}>
+        browse through music lyrics, without any ads!
+      </h1>
+      <br />
+      <form onSubmit={submitForm}>
+        <div className="justify-center" id="input">
+          <input
+            style={{ width: "900px" }}
+            className="rounded-2xl border-2 p-2 text-xl bg-black/60"
+            onChange={(e) => artistRef.current = e.target.value}
+            placeholder="Enter artist name (leave blank if unknown)"
+          />
+        </div><br></br>
+        <div className="justify-center" id="input" style={{ display: "flex" }}>
+          <input
+            style={{ width: "900px" }}
+            className="w-fit rounded-2xl border-2 p-2 text-xl bg-black/60"
+            onChange={(e) => nameRef.current = e.target.value}
+            placeholder="Enter song name"
+          />
+          <button
+            className="ml-2 align-middle border-2 rounded-3xl text-xl h-10 w-10 p-2 bg-black/60 hover:bg-black/20"
+            type="submit"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 16 16"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z"
+                clipRule="evenodd"
+              ></path>
+              <path
+                fillRule="evenodd"
+                d="M6.5 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zM13 6.5a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </form>
+    </div>
+  );
 }
